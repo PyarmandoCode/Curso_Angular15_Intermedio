051af2e5c91942c2e83f65bc22ec7fd3c2d577f1
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-02-16 09:16:35 -0500

    App-Angular-boostratp

--- app_discos/app_discos/src/app/app.module.ts
+++ app_discos/app_discos/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
